a252566735a2c6e34fa618b96c37eb5a71e621ea
Pinoberry <oscarjavier289@gmail@.com>
2024-05-09 18:21:37 -0400

    Agregando carrusel

--- darkmode.js
+++
@@ -1,8 +0,0 @@
-let toggle = document.getElementById('toggle');
-let label_toggle = document.getElementById('label__toggle')
-let fecha_divisora = document.querySelector('.fecha-divisor')
-
-toggle.addEventListener('change', (event) => {
-    let checked=event.target.checked;
-    document.body.classList.toggle('dark');
-} )
